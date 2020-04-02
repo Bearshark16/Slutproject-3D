8f04c05add04dc494f42a7dd6ec550e5356fbc14
Jacob Bekele Jansson <jacob.bekelejansson@elev.ga.ntig.se>
2020-04-02 17:30:19 +0200

    Item spawning
    Started creating a system for item spawning.

---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ItemSpawner : NetworkBehaviour
+{
+    [SerializeField]
+    public List<string> keys = new List<string>();
+    public List<GameObject> values = new List<GameObject>();
+
+    private Dictionary<string, GameObject> items = new Dictionary<string, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(keys.Count != 0 && values.Count != 0)
+        {
+            for (int i = 0; i < keys.Count; i++)
+            {
+                items.Add(keys[i], values[i]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
